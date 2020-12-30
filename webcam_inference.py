363bd73c27db83eb6788ec322bcc464f46324746
Thibault <thibo73800@gmail.com>
2020-12-30 09:38:32 +0100

    Fix memory grow when no GPU are avaiable

--- webcam_inference.py
+++ webcam_inference.py
@@ -46,7 +46,8 @@ def run_webcam_inference(detr):
 if __name__ == "__main__":
 
     physical_devices = tf.config.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    if len(physical_devices) == 1:
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     config = TrainingConfig()
     args = training_config_parser().parse_args()
